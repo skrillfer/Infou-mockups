86f2bec8f08f4e9df55e67e013df96d5b9c4d331
Luis Fernando Ramirez Santos <luisramirez@infoutilitygt.com>
2020-08-10 11:02:43 -0600

    add table and set responsive, add icon button add delete

--- src/app/pages/deal/deal.component.ts
+++ src/app/pages/deal/deal.component.ts
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 import { DealsService } from '../../services/deals.service';
 import { CartComponent } from '../cart/cart.component';
 import { ModalController } from '@ionic/angular';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-deal',
@@ -18,7 +19,22 @@ import { ModalController } from '@ionic/angular';
 export class DealComponent implements OnInit {
 
   deals: any = [];
+  profileForm = new FormGroup({
+    firstName: new FormControl('',[Validators.required]),
+    secondName: new FormControl(''),
+    othersName: new FormControl(''),
+    lastName: new FormControl('',[Validators.required]),
+    secondLastName: new FormControl('',),
 
+    nationality : new FormControl('',[Validators.required]),
+    email : new FormControl('',[Validators.required, Validators.email]),
+    cui : new FormControl('', [Validators.required]),
+    passport : new FormControl('',[Validators.required]),
+    phone : new FormControl('',[Validators.required]),
+    nit : new FormControl('',[Validators.required]),
+    password : new FormControl('',[Validators.required]),
+    confirmPassword : new FormControl('',[Validators.required])
+  });
   constructor(private dealsService: DealsService,
     private modalController: ModalController) { }
 
--- src/app/pages/deal/deal.module.ts
+++ src/app/pages/deal/deal.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { DealComponent } from './deal.component';
@@ -12,6 +12,7 @@ import { DealComponent } from './deal.component';
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
     RouterModule.forChild([
       {
         path: '',

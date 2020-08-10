218b7ec1724bb424cec64ba30c1e1cb9aaf9e2e6
Luis Fernando Ramirez Santos <luisramirez@infoutilitygt.com>
2020-08-09 19:51:36 -0600

    added profile

--- src/app/pages/account/account.component.ts
+++ src/app/pages/account/account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-account',
@@ -6,7 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account.component.scss'],
 })
 export class AccountComponent implements OnInit {
+  profileForm = new FormGroup({
+    firstName: new FormControl('',[Validators.required]),
+    secondName: new FormControl(''),
+    othersName: new FormControl(''),
+    lastName: new FormControl('',[Validators.required]),
+    secondLastName: new FormControl('',),
 
+    nationality : new FormControl('',[Validators.required]),
+    email : new FormControl('',[Validators.required, Validators.email]),
+    cui : new FormControl('', [Validators.required]),
+    passport : new FormControl('',[Validators.required]),
+    phone : new FormControl('',[Validators.required]),
+    nit : new FormControl('',[Validators.required]),
+    password : new FormControl('',[Validators.required]),
+    confirmPassword : new FormControl('',[Validators.required])
+  });
   constructor() { }
 
   ngOnInit() {}
